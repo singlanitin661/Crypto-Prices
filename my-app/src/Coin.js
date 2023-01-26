365aeb046acdd321944e491b1dbf2eee48cc3ed5
singlanitin661 <singlanitin661@gmail.com>
2023-01-26 18:02:18 +0530

    Completed

--- my-app/src/Coin.js
+++ my-app/src/Coin.js
@@ -5,11 +5,21 @@ function Coin({image,name,price,volume,pricechange,marketcap}) {
     <div className='coin-container'>
         <div className='coin-row'>
             <div className='coin'>
-                
-
-
+                <img src={image} alt="crypto" />
+                <h1>{name}</h1>
+                <p className="coin-symbol"></p>
             </div>
-
+            <div className="coin-data">
+              <p className="coin-price">Rs.{price}</p>
+              {pricechange<0 ?(
+                <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
+              ):(
+                <p className="coin-percent green">{pricechange.toFixed(2)}%</p>
+              )}
+              <p className="coin-marketcap">
+                Mkt Cap: Rs.{marketcap.toLocaleString()}
+              </p>
+            </div>
         </div>
 
 
